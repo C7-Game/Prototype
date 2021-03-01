2ca90b2f3892f1f05dc74a15b60e7a7499f19d88
Andrew <adj@fastmail.fm>
2021-02-28 20:35:59 -0500

    Load up the Civ III background art as the background of the main menu.
    Had to remove the button I'd added as I couldn't figure out how to get the background to be below the button if I added the button first.  But that's okay; we'll probably be adding things in a similar fashion anyway.
    
    I'm tempted to try to add an interactable button with PCX graphics to the main menu, since "how do we click on things?" seems like an important thing to understand.

--- C7/MainMenu.cs
+++ C7/MainMenu.cs
@@ -1,16 +1,35 @@
 using Godot;
 using System;
+using ReadCivData.ConvertCiv3Media;
 
 public class MainMenu : Node2D
 {
 	// Declare member variables here. Examples:
 	// private int a = 2;
 	// private string b = "text";
+	
+	public string Civ3Path = "D:/Civilization III";
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		GD.Print("Hello world!");
+		DisplayTitleScreen();
+	}
+	
+	private void DisplayTitleScreen()
+	{	
+		Pcx PcxTxtr = new Pcx(Civ3Path + "/Art/title.pcx");
+		ImageTexture Txtr = PCXToGodot.getImageTextureFromPCX(PcxTxtr);
+		
+		TextureRect texture = new TextureRect();
+		texture.Texture = Txtr;
+		AddChild(texture);
+ 
+		Button newButton = new Button();
+		newButton.Text = "Testing 123";
+		AddChild(newButton);
+		newButton.Connect("pressed", this, "_on_Button_pressed");
 	}
 
 //  // Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -19,7 +38,7 @@ public class MainMenu : Node2D
 //      
 //  }
 
-	private void _on_Button_pressed()
+	public void _on_Button_pressed()
 	{
 		GD.Print("Load button pressed");
 		GetTree().ChangeScene("res://C7Game.tscn");    
