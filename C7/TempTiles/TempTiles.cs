00aa15b01294f0c4dd03381169e59cf2ef84470e
Jim Nelson <jim@jimnelson.us>
2021-03-23 16:41:35 -0700

    minor refactor

--- C7/TempTiles/TempTiles.cs
+++ C7/TempTiles/TempTiles.cs
@@ -4,16 +4,18 @@ using System;
 public class TempTiles : Node2D
 {
     public string SavePath = Util.GetCiv3Path() + @"/Conquests/Saves";
+    private FileDialog Dialog;
+
     public override void _Ready()
     {
         GD.Print("TempTiles script started!");
+        Dialog = GetNode<FileDialog>("FileDialog");
+        Dialog.CurrentDir = SavePath;
     }
 
     public void _on_OpenFileButton_pressed()
     {
         GD.Print("Open button pressed!");
-        FileDialog Dialog = GetNode<FileDialog>("FileDialog");
-        Dialog.CurrentDir = SavePath;
         Dialog.Popup_();
     }
 }
