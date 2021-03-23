99bb5f57af576dce9a1a63e475cbfbae1b4fe847
Jim Nelson <jim@jimnelson.us>
2021-03-23 16:34:56 -0700

    Defaulting sane SAV folder

--- C7/TempTiles/TempTiles.cs
+++ C7/TempTiles/TempTiles.cs
@@ -3,16 +3,17 @@ using System;
 
 public class TempTiles : Node2D
 {
+    public string SavePath = Util.GetCiv3Path() + @"/Conquests/Saves";
     public override void _Ready()
     {
         GD.Print("TempTiles script started!");
-        string path = Util.GetCiv3Path() + @"/Conquests/Saves";
     }
 
     public void _on_OpenFileButton_pressed()
     {
         GD.Print("Open button pressed!");
         FileDialog Dialog = GetNode<FileDialog>("FileDialog");
+        Dialog.CurrentDir = SavePath;
         Dialog.Popup_();
     }
 }
