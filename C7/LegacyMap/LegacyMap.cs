07313d8b72c6bf1882470bc09b0ed428866faf20
myjimnelson <myjimnelson@users.noreply.github.com>
2021-03-28 20:27:45 -0700

    Ugly-ish but working LegacyMap base terrain viewer (#3)
    * renaming to OldLegacyMap
    
    * scene rename
    
    * New LegacyMap
    Hunting for data with offset +/- buttons
    
    * Moved debug text to TempTiles
    
    * Showing tiles from xpgc
    
    * All tiles showing? But offset issue
    
    * map looks right (all base terrain)
    
    * Got debug text printing over map
    
    * Fixed map width/height detection
    
    * Added ModRelPath vars but not reading

--- C7/LegacyMap/LegacyMap.cs
+++ C7/LegacyMap/LegacyMap.cs
@@ -9,24 +9,24 @@ public class LegacyMap : Node2D
     public interface ILegacyTile
     // Tiles need to provide this info to LegacyMap
     {
-        // temp hack for prototyping land/water only map
-        bool IsLand {get;}
-        int LegacyBaseTerrainID { get; }
-        int LegacyOverlayTerrainID { get; }
+        int LegacyFileID { get; }
+        int LegacyImageID { get; }
         int LegacyX {get;}
         int LegacyY {get;}
-        
-    }
+	}
     public IEnumerable<ILegacyTile> LegacyTiles;
-    private DynamicFont MapFont;
 	int[,] Map;
-	Hashtable Terrmask;
     TileMap TM;
+	TileSet TS;
+	private int[,] TileIDLookup;
+	// NOTE: The following two must be set externally before displaying map
+	public int MapWidth;
+	public int MapHeight;
+	// If a mod is in effect, set this, otherwise set to "" or "Conquests"
+	public string ModRelPath = "";
     public override void _Ready()
     {
-        string FontPath = Util.GetCiv3Path() + @"/LSANS.TTF";
-        MapFont = new DynamicFont();
-        MapFont.FontData = ResourceLoader.Load(FontPath) as DynamicFontData;
+		//
     }
 	public void TerrainAsTileMap() {
         if (TM != null) { RemoveChild(TM); }
@@ -34,81 +34,67 @@ public class LegacyMap : Node2D
 		TM = new TileMap();
 		TM.CellSize = new Vector2(64,32);
 		// TM.CenteredTextures = true;
-		TileSet TS = new TileSet();
+		TS = new TileSet();
 		TM.TileSet = TS;
 
-		Pcx PcxTxtr = new Pcx(Util.Civ3MediaPath("Art/Terrain/xpgc.pcx"));
-		ImageTexture Txtr = PCXToGodot.getImageTextureFromPCX(PcxTxtr);
-        // Quick hack to map graphic coordinate system to default BIQ terrain ID
-        Terrmask = new Hashtable();
-        Hashtable TerrID = new Hashtable
-        {
-            { 0, 1 },
-            { 1, 2 },
-            { 2, 11 },
-        };
+		TileIDLookup = new int[9,81];
 
 		int id = TS.GetLastUnusedTileId();
         // Make blank default tile
         // TODO: Make red tile or similar
         TS.CreateTile(id);
         id++;
-		for (int y = 0; y < PcxTxtr.Height; y += 64) {
-			for (int x = 0; x < PcxTxtr.Width; x+= 128, id++) {
-				TS.CreateTile(id);
-				TS.TileSetTexture(id, Txtr);
-				TS.TileSetRegion(id, new Rect2(x, y, 128, 64));
-				// order right, bottom, left, top; 0 is plains, 1 grass, 2 coast
-                // Temp hack: assuming 4-bit terrain IDs, bit-rotating them into one integer key
-                // TODO: Make the key an Array of IEquatable of some sort, most likely int or GUID
-				Terrmask.Add(
-					((int)TerrID[((y / 64) % 3)] << 12) +
-					((int)TerrID[((y / 64) / 3 % 3)] << 8) +
-					((int)TerrID[((x / 128) / 3 % 3)] << 4) +
-					(int)TerrID[((x / 128) % 3)]
-					, id);
-			}
-		}
 
-        // TODO: Don't hard-code size
-		int mywidth = 100, myheight = 100;
-		Map = new int[mywidth,myheight];
+		Map = new int[MapWidth,MapHeight];
 
 		// Populate map values
         if(LegacyTiles != null)
         {
             foreach (ILegacyTile tile in LegacyTiles)
             {
-                Map[tile.LegacyX,tile.LegacyY] = tile.LegacyBaseTerrainID;
+				// If tile media file not loaded yet
+				if(TileIDLookup[tile.LegacyFileID,1] == 0) { LoadTileSet(tile.LegacyFileID); }
+                Map[tile.LegacyX,tile.LegacyY] = TileIDLookup[tile.LegacyFileID,tile.LegacyImageID];
             }
         }
-		// Loop to lookup tile ids based on terrain mask
-		for (int y = 0; y < myheight; y++) {
-			for (int x = (1 - (y % 2)); x < mywidth; x+=2) {
-				int Top = y == 0 ? (Map[(x+1) % mywidth,y]) : (Map[x,y-1]);
-				int Bottom = y == myheight - 1 ? (Map[(x+1) % mywidth,y]) : (Map[x,y+1]);
-				int foo = 
-					((Map[(x+1) % mywidth,y]) << 12) +
-					(Bottom << 8) +
-					((Map[Mathf.Abs((x-1) % mywidth),y]) << 4) +
-					Top
-				;
-				try {
-				// Map[x,y] = (int)Terrmask["001001001001"];
-				Map[x,y] = (int)Terrmask[foo];
-				} catch { GD.Print(x + "," + y + " " + foo); }
-			}
-		}
-		// loop to place tiles, each of which contains 1/4 of 4 'real' map locations
-		for (int y = 0; y < myheight; y++) {
-			for (int x = 1 - (y%2); x < mywidth; x+=2) {
-				// TM.SetCellv(new Vector2(x + (y % 2), y), (new Random()).Next() % TS.GetTilesIds().Count);
-				// try {
+		for (int y = 0; y < MapHeight; y++) {
+			for (int x = y % 2; x < MapWidth; x+=2) {
 				TM.SetCellv(new Vector2(x, y), Map[x,y]);
-				// } catch {}
 			}
 		}
-        TM.Scale = new Vector2((float)0.2, (float)0.2);
+        // TM.Scale = new Vector2((float)0.2, (float)0.2);
 		AddChild(TM);
+	}
+	private void LoadTileSet(int fileID)
+	{
+		Hashtable FileNameLookup = new Hashtable
+		{
+			{ 0, "Art/Terrain/xtgc.pcx" },
+			{ 1, "Art/Terrain/xpgc.pcx" },
+			{ 2, "Art/Terrain/xdgc.pcx" },
+			{ 3, "Art/Terrain/xdpc.pcx" },
+			{ 4, "Art/Terrain/xdgp.pcx" },
+			{ 5, "Art/Terrain/xggc.pcx" },
+			{ 6, "Art/Terrain/wCSO.pcx" },
+			{ 7, "Art/Terrain/wSSS.pcx" },
+			{ 8, "Art/Terrain/wOOO.pcx" },
+		};
+		int id = TS.GetLastUnusedTileId();
+		// temp if
+		if(FileNameLookup[fileID] != null)
+		{
+		Pcx PcxTxtr = new Pcx(Util.Civ3MediaPath(FileNameLookup[fileID].ToString(), ModRelPath));
+		ImageTexture Txtr = PCXToGodot.getImageTextureFromPCX(PcxTxtr);
+
+		for (int y = 0; y < PcxTxtr.Height; y += 64) {
+			for (int x = 0; x < PcxTxtr.Width; x+= 128, id++) {
+				TS.CreateTile(id);
+				TS.TileSetTexture(id, Txtr);
+				TS.TileSetRegion(id, new Rect2(x, y, 128, 64));
+				GD.Print((x / 128) * (PcxTxtr.Height / 64) + (y / 64));
+				TileIDLookup[fileID, (x / 128) + (y / 64) * (PcxTxtr.Width / 128)] = id;
+			}
+		}
+		}
 	}
 }
--- C7/TempTiles/TempTiles.cs
+++ C7/TempTiles/TempTiles.cs
@@ -4,29 +4,76 @@ using System.Collections.Generic;
 
 public class TempTiles : Node2D
 {
+    private class TextLayerClass : Node2D
+    {
+        public List<TempTile> Tiles;
+
+        private DynamicFont MapFont;
+        public override void _Ready()
+        {
+            string FontPath = Util.GetCiv3Path() + @"/LSANS.TTF";
+            MapFont = new DynamicFont();
+            MapFont.FontData = ResourceLoader.Load(FontPath) as DynamicFontData;
+
+        }
+        public override void _Draw()
+        {
+            base._Draw();
+            MapFont.Size = 32;
+            if(Tiles != null)
+            {
+                foreach (TempTile tile in Tiles)
+                {
+                    // TODO: This is writing under the map and buttons for some reason
+                    DrawString(MapFont, new Vector2(tile.LegacyX * 64 + 32, tile.LegacyY * 32 + 80), tile.DebugByte.ToString(), new Color(0,0,0,1));
+                }
+            }
+        }
+    }
     private FileDialog Dialog;
     private QueryCiv3.Civ3File LegacyMapReader;
     private List<TempTile> Tiles;
     private class TempTile: LegacyMap.ILegacyTile
     {
-        public bool IsLand { get; set; }
+        /*
         public int LegacyBaseTerrainID { get; set; }
         public int LegacyOverlayTerrainID { get; set; }
+        */
         public int LegacyX { get; set; }
         public int LegacyY { get; set; }
+        public int LegacyFileID { get; set; }
+        public int LegacyImageID { get; set; }
+        public int DebugByte;
     }
     private LegacyMap MapUI;
+    private int TileOffset = 0;
+    private Button OffsetButton;
+    private DynamicFont MapFont;
+    private float ScaleFactor = (float)0.25;
+    private TextLayerClass DebugTextLayer;
+    private float MapAlpha = (float)1;
+
     public override void _Ready()
     {
+        string FontPath = Util.GetCiv3Path() + @"/LSANS.TTF";
+        MapFont = new DynamicFont();
+        MapFont.FontData = ResourceLoader.Load(FontPath) as DynamicFontData;
+
         // Create reference to child node so we can change its settings from here
         Dialog = GetNode<FileDialog>("FileDialog");
         Dialog.CurrentDir = Util.GetCiv3Path() + @"/Conquests/Saves";
         Dialog.Resizable = true;
 
+        OffsetButton = GetNode<Button>("OffsetButton");
+
         LegacyMapReader = new QueryCiv3.Civ3File();
         // Load LegacyMap scene (?) and attach to tree
         MapUI = new LegacyMap();
+        MapUI.Modulate = new Color(1,1,1,MapAlpha);
         this.AddChild(MapUI);
+        DebugTextLayer = new TextLayerClass();
+        DebugTextLayer.Scale = new Vector2(1, 1) * ScaleFactor;
+        this.AddChild(DebugTextLayer);
     }
 
     public void _on_OpenFileButton_pressed()
@@ -40,39 +87,64 @@ public class TempTiles : Node2D
         GetTree().Quit();
     }
 
+    public void _on_OffsetButton_pressed()
+    {
+        TileOffset++;
+        OffsetButton.Text = "Offset " + TileOffset.ToString();
+        CreateTileSet();
+        Update();
+    }
+    public void _on_OffsetMinusButton_pressed()
+    {
+        TileOffset--;
+        OffsetButton.Text = "Offset " + TileOffset.ToString();
+        CreateTileSet();
+        Update();
+    }
+
     public void _on_FileDialog_file_selected(string path)
     {
         LegacyMapReader.Load(path);
         CreateTileSet();
         MapUI.LegacyTiles = Tiles;
         MapUI.TerrainAsTileMap();
+        MapUI.Scale = new Vector2(1,1) * ScaleFactor;
+        Update();
     }
     private void CreateTileSet()
     {
+        // TODO: Pull mod path from embedded BIC if present
+        MapUI.ModRelPath = "";
         Tiles = new List<TempTile>();
         int Offset = LegacyMapReader.SectionOffset("WRLD", 2) + 8;
-        int WorldHeight = LegacyMapReader.ReadInt32(Offset);
-        int WorldWidth = LegacyMapReader.ReadInt32(Offset + 5*4);
+        MapUI.MapHeight = LegacyMapReader.ReadInt32(Offset);
+        MapUI.MapWidth = LegacyMapReader.ReadInt32(Offset + 5*4);
 
         Offset = LegacyMapReader.SectionOffset("TILE", 1);
-        for (int y=0; y < WorldHeight; y++)
+        for (int y=0; y < MapUI.MapHeight; y++)
         {
-            for (int x=y%2; x < WorldWidth; x+=2)
+            for (int x=y%2; x < MapUI.MapWidth; x+=2)
             {
                 TempTile ThisTile = new TempTile();
                 ThisTile.LegacyX = x;
                 ThisTile.LegacyY = y;
 
+                /*
                 int TerrainByte = LegacyMapReader.ReadByte(Offset+53);
                 ThisTile.LegacyBaseTerrainID = TerrainByte & 0x0F;
                 ThisTile.LegacyOverlayTerrainID = TerrainByte >> 4;
-                // If low nybble of terrain byte is < 11, tile is land
-                ThisTile.IsLand = ThisTile.LegacyBaseTerrainID < 11;
+                */
+                ThisTile.DebugByte = LegacyMapReader.ReadByte(Offset+TileOffset);
+                ThisTile.LegacyFileID = LegacyMapReader.ReadByte(Offset+17);
+                ThisTile.LegacyImageID = LegacyMapReader.ReadByte(Offset+16);
 
                 Tiles.Add(ThisTile);
                 // 212 bytes per tile in Conquests SAV
                 Offset += 212;
             }
         }
+        DebugTextLayer.Visible = TileOffset != 0;
+        DebugTextLayer.Tiles = Tiles;
+        DebugTextLayer.Update();
     }
 }
