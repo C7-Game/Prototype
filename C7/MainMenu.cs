c66832717b2384afb90573f336d8440639e69d01
Andrew <adj@fastmail.fm>
2021-11-01 01:26:36 -0400

    Add all the buttons on the main menu, and switch the label of the existing one to New Game.
    Right now they all start a new game, so it doesn't do much yet, but makes the menu look a bit more menu-y, and shows that I figured out how to position child nodes on the scene.

--- C7/MainMenu.cs
+++ C7/MainMenu.cs
@@ -27,9 +27,65 @@ public class MainMenu : Node2D
 		AddChild(texture);
  
 		Button newButton = new Button();
-		newButton.Text = "Testing 123";
+		newButton.Text = "New Game";
+		newButton.SetPosition(new Vector2(860, 160));
 		AddChild(newButton);
 		newButton.Connect("pressed", this, "_on_Button_pressed");
+		
+		Button quickStart = new Button();
+		quickStart.Text = "Quick Start";
+		quickStart.SetPosition(new Vector2(860, 195));
+		AddChild(quickStart);
+		quickStart.Connect("pressed", this, "_on_Button_pressed");
+		
+		Button tutorial = new Button();
+		tutorial.Text = "Tutorial";
+		tutorial.SetPosition(new Vector2(860, 230));
+		AddChild(tutorial);
+		tutorial.Connect("pressed", this, "_on_Button_pressed");
+		
+		Button loadGame = new Button();
+		loadGame.Text = "Load Game";
+		loadGame.SetPosition(new Vector2(860, 265));
+		AddChild(loadGame);
+		loadGame.Connect("pressed", this, "_on_Button_pressed");
+		
+		Button loadScenario = new Button();
+		loadScenario.Text = "Load Scenario";
+		loadScenario.SetPosition(new Vector2(860, 300));
+		AddChild(loadScenario);
+		loadScenario.Connect("pressed", this, "_on_Button_pressed");
+		
+		Button hallOfFame = new Button();
+		hallOfFame.Text = "Hall of Fame";
+		hallOfFame.SetPosition(new Vector2(860, 335));
+		AddChild(hallOfFame);
+		hallOfFame.Connect("pressed", this, "_on_Button_pressed");
+		
+		Button preferences = new Button();
+		preferences.Text = "Preferences";
+		preferences.SetPosition(new Vector2(860, 370));
+		AddChild(preferences);
+		preferences.Connect("pressed", this, "_on_Button_pressed");
+		
+		Button audioPreferences = new Button();
+		audioPreferences.Text = "Audio Preferences";
+		audioPreferences.SetPosition(new Vector2(860, 405));
+		AddChild(audioPreferences);
+		audioPreferences.Connect("pressed", this, "_on_Button_pressed");
+		
+		Button credits = new Button();
+		credits.Text = "Credits";
+		credits.SetPosition(new Vector2(860, 440));
+		AddChild(credits);
+		credits.Connect("pressed", this, "_on_Button_pressed");
+		
+		Button exit = new Button();
+		exit.Text = "Exit";
+		exit.SetPosition(new Vector2(860, 475));
+		AddChild(exit);
+		exit.Connect("pressed", this, "_on_Button_pressed");
+		
 	}
 
 //  // Called every frame. 'delta' is the elapsed time since the previous frame.
