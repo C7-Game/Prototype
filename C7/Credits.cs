239a84e422a270e2bdc0a6c39364806463030b32
Quintillus <quintillus@warpmail.net>
2021-11-01 04:34:21 -0400

    Add a return-to-main-menu button on the credits so you aren't stuck there forever, as well as the first credit in the new version of the game.

--- C7/Credits.cs
+++ C7/Credits.cs
@@ -19,5 +19,26 @@ public class Credits : Node2D
 		TextureRect CreditsBackground = new TextureRect();
 		CreditsBackground.Texture = CreditsTexture;
 		AddChild(CreditsBackground);
+
+		//Todo: Either import Text/credits.txt and scroll it as done in-game, or have our own custom credits.
+		Label creditsLabel = new Label();
+		creditsLabel.AddColorOverride("font_color", new Color(0, 0, 0));
+		creditsLabel.Text = "Project Ringleader: WildWeazel";
+		creditsLabel.SetPosition(new Vector2(360, 120));
+		AddChild(creditsLabel);
+
+		Pcx GoBackPCX = new Pcx(Util.Civ3MediaPath("Art/exitBox-backgroundStates.pcx"));
+		ImageTexture GoBackTexture = PCXToGodot.getImageTextureFromPCX(GoBackPCX, 0, 0, 72, 48);
+
+		TextureButton GoBackButton = new TextureButton();
+		GoBackButton.TextureNormal = GoBackTexture;
+		GoBackButton.SetPosition(new Vector2(952, 720));
+		AddChild(GoBackButton);
+		GoBackButton.Connect("pressed", this, "ReturnToMenu");
+	}
+	public void ReturnToMenu()
+	{
+		GD.Print("Returning to main menu");
+		GetTree().ChangeScene("res://MainMenu.tscn");    
 	}
 }
