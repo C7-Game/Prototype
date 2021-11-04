0c47f617e7709b95a02d4999fd9f636651b81a63
Jim Nelson <jim@jimnelson.us>
2021-11-04 02:08:22 -0700

    mapgen mocking up  tile fills

--- C7/Experiments/MapGenTest.cs
+++ C7/Experiments/MapGenTest.cs
@@ -16,20 +16,61 @@ public class MapGenTest : Node2D
     public override void _Ready()
     {
         MapNode = new LegacyMap();
+        MapNode.Position = new Vector2(-32, -16);
         AddChild(MapNode);
         GenerateMap();
     }
     protected void GenerateMap()
     {
         GD.Print("generating map");
-        int mapWidth = 4;
-        int mapHeight = 4;
-        MapNode.MapWidth = mapWidth;
-        MapNode.MapHeight = mapHeight;
-        MapTiles = new List<GenTile>();
+        int mapWidth = 32;
+        int mapHeight = 24;
+        OpenSimplexNoise simplex = new OpenSimplexNoise();
+        int[,] noiseMap = new int[mapWidth, mapHeight];
         for (int x=0; x<mapWidth; x++)
         {
             for (int y=0; y<mapHeight; y++)
+            {
+                float noise = simplex.GetNoise2d(x, y);
+                int value;
+                if (noise < (float)0) value = 13;
+                else value = 0;
+                noiseMap[x,y] = value;
+            }
+
+        }
+
+        MapNode.MapWidth = mapWidth;
+        MapNode.MapHeight = mapHeight;
+        MapTiles = new List<GenTile>();
+		for (int y = 0; y < mapHeight; y++) {
+			for (int x = (y % 2); x < mapWidth; x+=2) {
+				int Top = y == 0 ? (noiseMap[(x+1) % mapWidth,y]) : (noiseMap[x,y-1]);
+				int Bottom = y == mapHeight - 1 ? (noiseMap[(x+1) % mapWidth,y]) : (noiseMap[x,y+1]);
+                /*
+				string foo = 
+					(noiseMap[(x+1) % mapWidth,y]).ToString("D3") +
+					Bottom.ToString("D3") +
+					(noiseMap[Mathf.Abs((x-1) % mapWidth),y]).ToString("D3") +
+					Top.ToString("D3")
+				;
+				try {
+				// noiseMap[x,y] = (int)Terrmask["001001001001"];
+				noiseMap[x,y] = (int)Terrmask[foo];
+				} catch { GD.Print(x + "," + y + " " + foo); }
+                */
+                GenTile newTile = new GenTile();
+                newTile.LegacyX = x;
+                newTile.LegacyY = y;
+                newTile.LegacyFileID = 1;
+                newTile.LegacyImageID = 1;
+                MapTiles.Add(newTile);
+			}
+		}
+        /*
+        for (int y=0; y<mapHeight; y++)
+        {
+            for (int x=(y%2); x<mapWidth; x+=2)
             {
                 GenTile newTile = new GenTile();
                 newTile.LegacyX = x;
@@ -40,6 +81,7 @@ public class MapGenTest : Node2D
                 MapTiles.Add(newTile);
             }
         }
+        */
         MapNode.LegacyTiles = MapTiles;
         MapNode.TerrainAsTileMap();
     }
