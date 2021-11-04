3c7a5ca67ad0a8300d865ff005dec85617948687
Quintillus <quintillus@warpmail.net>
2021-11-04 00:54:10 -0400

    Center the domestic advisor.

--- C7/Game.cs
+++ C7/Game.cs
@@ -74,6 +74,14 @@ public class Game : Node2D
 				AdvisorContainer = GetNode<CenterContainer>("CanvasLayer/Advisor");
 				DomesticAdvisor advisor = new DomesticAdvisor();
 				AdvisorContainer.AddChild(advisor);
+
+				//Center the advisor container.  Following directions at https://docs.godotengine.org/en/stable/tutorials/gui/size_and_anchors.html?highlight=anchor
+				//Also taking advantage of it being 1024x768, as the directions didn't really work.  This is not 100% ideal (would be great for a general-purpose solution to work),
+				//but does work with the current graphics independent of resolution.
+				AdvisorContainer.MarginLeft = -512;
+				AdvisorContainer.MarginRight = -512;
+				AdvisorContainer.MarginTop = -384;
+				AdvisorContainer.MarginBottom = 384;
 			}
 			else {
 				GD.Print("Showing advisor");
