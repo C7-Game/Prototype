e322b71097a835341e1f22ebd6f186e658829db1
Quintillus <quintillus@warpmail.net>
2021-11-09 01:15:51 -0500

    Forgot to commit the clearing of the wait queue.  How'd that escape TortoiseGit?  Well, it could have been a lot worse.

--- C7Engine/EntryPoints/TurnHandling.cs
+++ C7Engine/EntryPoints/TurnHandling.cs
@@ -6,10 +6,14 @@ namespace C7Engine
         public static void EndTurn()
         {
             GameData gameData = EngineStorage.gameData;
+            //Reset movement points available for all units
             foreach (MapUnit mapUnit in gameData.mapUnits)
             {
                 mapUnit.movementPointsRemaining = mapUnit.unitType.movement;
             }
+            //Clear all wait queue, so if a player ended the turn without handling all waited units, they are selected
+            //at the same place in the order.  Confirmed this is what Civ3 does.
+            UnitInteractions.ClearWaitQueue();
             gameData.turn++;
         }
     }
