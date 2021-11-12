fc3e1c4ca269e2e5d6e8e0e73d672cce25d3d55f
Quintillus <quintillus@warpmail.net>
2021-11-12 02:01:16 -0500

    Added music to the main menu.  It'll automatically load Menu1.mp3 and play it.  Better yet, it works even if it's a variable bit rate (VBR) mp3.  Tested with an mp3 version of Baba Yetu from Chris's CD (the Civ IV version is 128 kbps, so it wouldn't have tested that feature).

---
+++ C7/MainMenuMusicPlayer.cs
@@ -0,0 +1,32 @@
+using Godot;
+using System;
+
+public class MainMenuMusicPlayer : AudioStreamPlayer
+{
+	const bool musicEnabled = false;
+
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		//Figured out how to load the mp3 from this post: https://godotengine.org/qa/30210/how-do-load-resource-works
+
+		string mp3Path = Util.Civ3MediaPath("Sounds/Menu/Menu1.mp3");
+		File mp3File = new File();
+		mp3File.Open(mp3Path, Godot.File.ModeFlags.Read);
+
+		AudioStreamMP3 mp3 = new AudioStreamMP3();
+		long fileSize = (long)mp3File.GetLen();	//might blow up if it's > 2 GB, oh well
+		mp3.Data = mp3File.GetBuffer(fileSize);
+
+		this.Stream = mp3;
+
+		if (musicEnabled) {
+			this.Play();
+		}
+	}
+	
+	private void OnMP3Finished()
+	{
+		this.Play();
+	}
+}
