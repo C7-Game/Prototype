3095bf22ef6d960ca448283b67c9548f7e72666c
Quintillus <quintillus@warpmail.net>
2021-11-12 02:47:42 -0500

    Accidentally enabled music by default in the last commit.  Whoopsies.

--- C7/MainMenuMusicPlayer.cs
+++ C7/MainMenuMusicPlayer.cs
@@ -3,7 +3,7 @@ using System;
 
 public class MainMenuMusicPlayer : AudioStreamPlayer
 {
-	const bool musicEnabled = true;
+	const bool musicEnabled = false;
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
