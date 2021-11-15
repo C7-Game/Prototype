89e98dab42f1e70cb21d7989498e06cb4ad08892
Quintillus <quintillus@warpmail.net>
2021-11-15 00:26:23 -0500

    Refactor the background of a popup so it's accessible to any popup via a static method in PopupOverlay.cs, and so that it only adds one child to the scene rather than a whole bunch.

--- C7/UIElements/Popups/DisbandConfirmation.cs
+++ C7/UIElements/Popups/DisbandConfirmation.cs
@@ -46,41 +46,16 @@ public class DisbandConfirmation : TextureRect
 		AdvisorHead.SetPosition(new Vector2(375, 0));
 		AddChild(AdvisorHead);
 
-		//The pop-up part is the tricky part
-		ImageTexture topLeftPopup = Util.LoadTextureFromPCX("Art/popupborders.pcx", 251, 1, 61, 44);
-		ImageTexture topCenterPopup = Util.LoadTextureFromPCX("Art/popupborders.pcx", 313, 1, 61, 44);
-		ImageTexture topRightPopup = Util.LoadTextureFromPCX("Art/popupborders.pcx", 375, 1, 61, 44);
-		ImageTexture middleLeftPopup = Util.LoadTextureFromPCX("Art/popupborders.pcx", 251, 46, 61, 44);
-		ImageTexture middleCenterPopup = Util.LoadTextureFromPCX("Art/popupborders.pcx", 313, 46, 61, 44);
-		ImageTexture middleRightPopup = Util.LoadTextureFromPCX("Art/popupborders.pcx", 375, 46, 61, 44);
-		ImageTexture bottomLeftPopup = Util.LoadTextureFromPCX("Art/popupborders.pcx", 251, 91, 61, 44);
-		ImageTexture bottomCenterPopup = Util.LoadTextureFromPCX("Art/popupborders.pcx", 313, 91, 61, 44);
-		ImageTexture bottomRightPopup = Util.LoadTextureFromPCX("Art/popupborders.pcx", 375, 91, 61, 44);
-
-		//Dimensions are 530x320.  The leaderhead takes up 110.  So the popup is 530x210.
-		//We have multiples of... 62? For the horizontal dimension, 45 for vertical.
-		//45 does not fit into 210.  90, 135, 180, 215.  Well, 215 is sorta closeish.
-		//62, we got 62, 124, 248, 496, 558.  Doesn't match up at all.
-		//Which means that partial textures can be used.  Lovely.
-
-		//Let's try adding some helper functions so this can be refactored later into a more general-purpose popup popper
-		int vOffset = 110;
-		int height = 320;
-		drawRowOfPopup(vOffset, 530, topLeftPopup, topCenterPopup, topRightPopup);
-		const int VTILE_SIZE = 44;
-		vOffset+=VTILE_SIZE;
-		for (;vOffset < height - VTILE_SIZE; vOffset += VTILE_SIZE) {
-			drawRowOfPopup(vOffset, 530, middleLeftPopup, middleCenterPopup, middleRightPopup);
-		}
-		vOffset = height - VTILE_SIZE;
-		drawRowOfPopup(vOffset, 530, bottomLeftPopup, bottomCenterPopup, bottomRightPopup);
-
+		TextureRect background = PopupOverlay.GetPopupBackground(530, 210);
+		background.SetPosition(new Vector2(0, 110));
+		AddChild(background);
 
 		//Pop-up done.  Should refactor it someday so it's reusable.  But for now let's add the other things and stuff
 		HBoxContainer header = new HBoxContainer();
 		header.Alignment = BoxContainer.AlignMode.Center;
 		Label advisorType = new Label();
 		advisorType.AddColorOverride("font_color", new Color(0, 0, 0));
+
 		//Set the font size.  For labels, there is no one-off override, so we have to
 		//set it on a theme like this.
 		//The SetFont arguments aren't documented in a way that a non-Godot expert can understand
@@ -97,7 +72,7 @@ public class DisbandConfirmation : TextureRect
 		header.SetPosition(new Vector2(0, 120));
 		header.AnchorLeft = 0.0f;
 		header.AnchorRight = 1.0f;
-		header.MarginRight = 10;    //For some reason this isn't causing it to be indented 10 pixels from the right.  Uncomment the line above and you'll see the tooltip goes all the way across
+		header.MarginRight = 10;    //For some reason this isn't causing it to be indented 10 pixels from the right.
 		AddChild(header);
 
 		Label warningMessage = new Label();
@@ -106,22 +81,9 @@ public class DisbandConfirmation : TextureRect
 		//Maybe there's an awesomer control we can user instead
 		warningMessage.AddColorOverride("font_color", new Color(0, 0, 0));
 		warningMessage.Text = "Disband Settler?  Pardon me but these are OUR people. Do \nyou really want to disband them?";
-		
-		// HBoxContainer messageCenteringContainer = new HBoxContainer();
-		// messageCenteringContainer.Alignment = BoxContainer.AlignMode.Center;
-		// messageCenteringContainer.SetPosition(new Vector2(0, 170));
-		// messageCenteringContainer.AnchorLeft = 0.05f;
-		// messageCenteringContainer.AnchorRight = 0.95f;
-		// messageCenteringContainer.AddChild(warningMessage);
-		// AddChild(messageCenteringContainer);
 
 		warningMessage.SetPosition(new Vector2(25, 170));
 		AddChild(warningMessage);
-
-		//30, 215
-		//50, 215
-
-		//and 245
 
 		AddButton("Yes, we need to!", 215, "disband");
 		AddButton("No. Maybe you are right, advisor.", 245, "cancel");
@@ -157,7 +119,6 @@ public class DisbandConfirmation : TextureRect
 		newButtonLabel.Text = label;
 
 		newButtonLabel.AddColorOverride("font_color", new Color(0, 0, 0));
-		//Only the Exit and part of the Credits button are getting the right color.  The rest are black.  Not sure why.
 		newButtonLabel.AddColorOverride("font_color_hover", Color.Color8(255, 0, 0));
 		newButtonLabel.AddColorOverride("font_color_pressed", Color.Color8(0, 255, 0));	//when actively being clicked
 		//Haven't figured out how to set the color after you've clicked on something (i.e. made it focused)
@@ -169,30 +130,5 @@ public class DisbandConfirmation : TextureRect
 		newButtonLabel.SetPosition(new Vector2(HORIZONTAL_POSITION + 25, verticalPosition + BUTTON_LABEL_OFFSET));
 		this.AddChild(newButtonLabel);
 		newButtonLabel.Connect("pressed", this, actionName);
-	}
-
-	private void drawRowOfPopup(int vOffset, int width, ImageTexture left, ImageTexture center, ImageTexture right)
-	{
-		//Okay, at least we only need one function for all three rows, it can be SIMD (single instruction, multiple data) by analogy
-		TextureRect leftRectangle = new TextureRect();
-		leftRectangle.SetPosition(new Vector2(0, vOffset));
-		leftRectangle.Texture = left;
-		AddChild(leftRectangle);
-
-		const int TILE_SIZE = 61;   //yes, it will always be 61.  at least with Civ graphics.  so like WildWeazel, it will be hard coded
-		int leftOffset = TILE_SIZE;
-		for (;leftOffset < width - TILE_SIZE; leftOffset += TILE_SIZE)
-		{
-			TextureRect middleRectangle = new TextureRect();
-			middleRectangle.SetPosition(new Vector2(leftOffset, vOffset));
-			middleRectangle.Texture = center;
-			AddChild(middleRectangle);
-		}
-
-		leftOffset = width - TILE_SIZE;
-		TextureRect rightRectangle = new TextureRect();
-		rightRectangle.SetPosition(new Vector2(leftOffset, vOffset));
-		rightRectangle.Texture = right;
-		AddChild(rightRectangle);
 	}
 }
--- C7/UIElements/Popups/PopupOverlay.cs
+++ C7/UIElements/Popups/PopupOverlay.cs
@@ -1,10 +1,13 @@
 using Godot;
-using System;
+using ConvertCiv3Media;
 
 public class PopupOverlay : HBoxContainer
 {
 	[Signal] public delegate void UnitDisbanded();
 	Control currentChild = null;
+	
+	const int HTILE_SIZE = 61;
+	const int VTILE_SIZE = 44;
 
 	public enum PopupCategory {
 		Advisor,
@@ -57,6 +60,66 @@ public class PopupOverlay : HBoxContainer
 			GD.PrintErr("Received request to show invalid dialog type " + dialogType);
 		}
 	}
+
+	public static TextureRect GetPopupBackground(int width, int height)
+	{		
+		Image image = new Image();
+		image.Create(width, height, false, Image.Format.Rgba8);
+
+		Pcx popupborders = new Pcx(Util.Civ3MediaPath("Art/popupborders.pcx"));
+
+		//The pop-up part is the tricky part
+		Image topLeftPopup      = PCXToGodot.getImageFromPCX(popupborders, 251, 1, 61, 44);
+		Image topCenterPopup    = PCXToGodot.getImageFromPCX(popupborders, 313, 1, 61, 44);
+		Image topRightPopup     = PCXToGodot.getImageFromPCX(popupborders, 375, 1, 61, 44);
+		Image middleLeftPopup   = PCXToGodot.getImageFromPCX(popupborders, 251, 46, 61, 44);
+		Image middleCenterPopup = PCXToGodot.getImageFromPCX(popupborders, 313, 46, 61, 44);
+		Image middleRightPopup  = PCXToGodot.getImageFromPCX(popupborders, 375, 46, 61, 44);
+		Image bottomLeftPopup   = PCXToGodot.getImageFromPCX(popupborders, 251, 91, 61, 44);
+		Image bottomCenterPopup = PCXToGodot.getImageFromPCX(popupborders, 313, 91, 61, 44);
+		Image bottomRightPopup  = PCXToGodot.getImageFromPCX(popupborders, 375, 91, 61, 44);
+
+		//Dimensions are 530x320.  The leaderhead takes up 110.  So the popup is 530x210.
+		//We have multiples of... 62? For the horizontal dimension, 45 for vertical.
+		//45 does not fit into 210.  90, 135, 180, 215.  Well, 215 is sorta closeish.
+		//62, we got 62, 124, 248, 496, 558.  Doesn't match up at all.
+		//Which means that partial textures can be used.  Lovely.
+
+		//Let's try adding some helper functions so this can be refactored later into a more general-purpose popup popper
+		int vOffset = 0;
+		drawRowOfPopup(image, vOffset, width, topLeftPopup, topCenterPopup, topRightPopup);
+		vOffset+=VTILE_SIZE;
+		for (;vOffset < height - VTILE_SIZE; vOffset += VTILE_SIZE) {
+			drawRowOfPopup(image, vOffset, width, middleLeftPopup, middleCenterPopup, middleRightPopup);
+		}
+		vOffset = height - VTILE_SIZE;
+		drawRowOfPopup(image, vOffset, width, bottomLeftPopup, bottomCenterPopup, bottomRightPopup);
+
+		ImageTexture texture = new ImageTexture();
+		texture.CreateFromImage(image);
+
+		TextureRect rect = new TextureRect();
+		rect.Texture = texture;
+
+		return rect;
+	}
+	
+
+	private static void drawRowOfPopup(Image image, int vOffset, int width, Image left, Image center, Image right)
+	{
+
+		image.BlitRect(left, new Rect2(new Vector2(0, 0), new Vector2(left.GetWidth(), left.GetHeight())), new Vector2(0, vOffset));
+
+		int leftOffset = HTILE_SIZE;
+		for (;leftOffset < width - HTILE_SIZE; leftOffset += HTILE_SIZE)
+		{
+			image.BlitRect(center, new Rect2(new Vector2(0, 0), new Vector2(center.GetWidth(), center.GetHeight())), new Vector2(leftOffset, vOffset));
+		}
+
+		leftOffset = width - HTILE_SIZE;
+		image.BlitRect(right, new Rect2(new Vector2(0, 0), new Vector2(right.GetWidth(), right.GetHeight())), new Vector2(leftOffset, vOffset));
+	}
+	
 }
 
 
