bb1a4e68f5cd98f5ccf61b761064e32e4b473b95
Quintillus <quintillus@warpmail.net>
2021-11-24 02:00:44 -0500

    Speed up FLC import by 40%, from a bit over 700 ms to 420 ms, by switching from SetPixel to LoadBmpFromBuffer.
    SetPixel was taking up half the time before, about 350 ms out of just over 700.  This new method reduces that time by 80%.
    
    One major call-out about this code is that it uses an unsafe helper function to process the bits, including pointers and pointer arithmetic.  When thinking "how do we create a byte buffer where there are various length primitives included in it?" this seemed like the way to go, as it allows us to treat shorts and ints as shorts and ints by pointer casting.
    
    We might be able to work around it without an unsafe block by tossing the ints/shorts in the header in as multiple bytes, based on their constituent bytes, and using bytes from the palette as-is.  But that introduces a different level of complex reasoning.
    
    Regardless of what we decide about the specifics of this function, it shows that there are real and significant gains to be had by using byte buffers instead of SetPixel, and it was fun to dive back into the BMP format and learn how to write some low-level C-style C#.

--- C7/Civ3Unit/Civ3Unit.cs
+++ C7/Civ3Unit/Civ3Unit.cs
@@ -92,25 +92,71 @@ public class Civ3Unit : Civ3UnitSprite
         Image OutImage = new Image();
         OutImage.Create(width, height, false, Image.Format.Rgba8);
         OutImage.Lock();
-        int c = 0;
-        for (int y = 0; y < height; y++)
-        {
-            for (int x = 0; x < width; x++)
-            {
-                if (shadows && colorIndices[c] > 239) {
-                    // using black and transparency
-                    // OutImage.SetPixel(x, y, Color.Color8(0,0,0, (byte)((255 -colorIndices[c]) << 4)));
-                    // using the palette color but adding transparency
-                    // OutImage.SetPixel(i % width, i / width, Color.Color8(palette[ba[i],0], palette[ba[i],1], palette[ba[i],2], (byte)((255 -ba[i]) * 16)));
-                } else {
-                    // OutImage.SetPixel(x, y, Color.Color8(palette[colorIndices[c],0], palette[colorIndices[c],1], palette[colorIndices[c],2], colorIndices[c] == 255 ? (byte)0 : (byte)255));
-                }
-                c++;
-            }
-        }
+        byte[] bmpBuffer = getBmpBuffer(colorIndices, palette, width, height, transparent, shadows);
+        OutImage.LoadBmpFromBuffer(bmpBuffer);
         OutImage.Unlock();
 
         return OutImage;
+    }
+
+    public static unsafe byte[] getBmpBuffer(byte[] colorIndices, byte[,] palette, int width, int height, int[] transparent = null, bool shadows = false) {
+        
+        int bmpSize = width * height * 4 + 54;  //54 = Windows 3 BMP header size.
+        byte[] bmpBuffer = new byte[bmpSize];
+        fixed (byte* fPtr = bmpBuffer) {
+            //BMP header.  This is a mix of byte, short, and int.  Probably a cleaner way to do this, but my C is rusty.
+            byte* bPtr = fPtr;
+            int* iPtr;
+            short* sPtr;
+            bPtr[0] = 66;    //B
+            bPtr[1] = 77;    //M
+            bPtr+=2;
+            iPtr = (int*)bPtr;
+            iPtr[0] = bmpSize;  //size of BMP file in bytes
+            iPtr[1] = 0;    //reserved, two shorts
+            iPtr[2] = 54;   //offset of image data from start of file
+            iPtr[3] = 40;   //size of Windows 3 BMP header
+            iPtr[4] = width;
+            iPtr[5] = height;
+            bPtr+=24;   //6 * 4
+            sPtr = (short*)bPtr;
+            sPtr[0] = 1;    //num color planes
+            sPtr[1] = 32;   //bit depth.  we want 32-bit with alpha
+            bPtr+=4;
+            iPtr = (int*)bPtr;
+            iPtr[0] = 0;    //compression - none.  Godot doesn't support compression
+            iPtr[1] = bmpSize - 54; //size, without headers
+            iPtr[2] = 0;    //horizontal resolution.  ignored.
+            iPtr[3] = 0;    //vertical resolution.  ignored.
+            iPtr[4] = 0;    //num colors in palette.  not relevant for 32-bit
+            iPtr[5] = 0;    //num important colors in palette.  not relevant.
+            bPtr+=24;   //6 * 4
+            iPtr = (int*)bPtr;
+            //Ready for image data
+
+            int c = 0;
+            //The BMP data is stored bottom-to-top, whereas our data is top-to-bottom
+            //Thus we'll use c to calculate the index for each row
+            for (int y = height - 1; y > -1; y--)
+            {
+                c = width * y;
+                for (int x = 0; x < width; x++)
+                {
+                    if (shadows && colorIndices[c] > 239) {
+                        // using black and transparency
+                        iPtr[0] = ((255 - colorIndices[c]) << 4) << 24;
+                        // using the palette color but adding transparency
+                        // OutImage.SetPixel(i % width, i / width, Color.Color8(palette[ba[i],0], palette[ba[i],1], palette[ba[i],2], (byte)((255 -ba[i]) * 16)));
+                    } else {
+                        // A, R, G, B
+                        iPtr[0] = ((byte)255) << 24 | palette[colorIndices[c], 0] << 16 | palette[colorIndices[c], 1] << 8 | palette[colorIndices[c], 2] << 0;
+                    }
+                    c++;
+                    iPtr++;
+                }
+            }
+        }
+        return bmpBuffer;
     }
 }
 // This is just to add some movement to an AnimatedSprite
