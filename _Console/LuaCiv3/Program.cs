667b03374666243b8803ebb0f73e88f2191fe371
Jim Nelson <jim@jimnelson.us>
2021-11-26 01:01:22 -0800

    stubbing out console program to read map

---
+++ _Console/LuaCiv3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+using QueryCiv3;
+using MoonSharp.Interpreter;
+
+namespace LuaCiv3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Script lua = new Script();
+            string runMe = File.ReadAllText("civ3map.lua");
+
+            DynValue _ = lua.DoString(runMe);
+
+            byte[] defaultBicBytes = QueryCiv3.Util.ReadFile(Util.GetCiv3Path() + @"/Conquests/conquests.biq");
+    		mapReader = new QueryCiv3.SavData(QueryCiv3.Util.ReadFile(path), defaultBicBytes);
+        }
+    }
+}
