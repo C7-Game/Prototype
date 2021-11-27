17f10f0e7108c3c1604fe80f58ddc90d9abd68ce
Jim Nelson <jim@jimnelson.us>
2021-11-26 20:42:55 -0800

    Cleaned up console script to run importer

--- _Console/BuildDevSave/Program.cs
+++ _Console/BuildDevSave/Program.cs
@@ -2,7 +2,6 @@
 using System.IO;
 using System.Text.Json;
 using QueryCiv3;
-using MoonSharp.Interpreter;
 using C7GameData;
 
 namespace LuaCiv3
@@ -15,17 +14,8 @@ namespace LuaCiv3
         static string SavFilePath { get => @"/Conquests/Saves/for-c7-seed-1234567.SAV"; }
         static void Main(string[] args)
         {
-
-            RegisterQueryCiv3Types();
-            Script lua = new Script();
-            string runMe = File.ReadAllText("civ3map.lua");
-
-            DynValue _ = lua.DoString(runMe);
-
             byte[] defaultBicBytes = QueryCiv3.Util.ReadFile(GetCiv3Path + @"/Conquests/conquests.biq");
     		SavData mapReader = new QueryCiv3.SavData(QueryCiv3.Util.ReadFile(GetCiv3Path + SavFilePath), defaultBicBytes);
-
-            _ = lua.Call(lua.Globals["process_sav"], mapReader);
 
             JsonSerializerOptions jsonOptions = new JsonSerializerOptions
             {
@@ -33,41 +23,12 @@ namespace LuaCiv3
                 PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
                 // Pretty print because...well just because, OK?
                 WriteIndented = true,
+                // By default it only serializes getters, this makes it serialize fields, too
                 IncludeFields = true,
             };
             C7SaveFormat output = ImportCiv3.ImportSav(GetCiv3Path + SavFilePath, GetCiv3Path + @"/Conquests/conquests.biq");
             string foo = JsonSerializer.Serialize(output, jsonOptions);
             Console.WriteLine(foo);
-        }
-
-        // Enables these type instances to be accessed directly by Lua
-        static void RegisterQueryCiv3Types() {
-            UserData.RegisterType<SavData>();
-            UserData.RegisterType<BicData>();
-            UserData.RegisterType<Civ3File>();
-            UserData.RegisterType<GameSection>();
-            UserData.RegisterType<WrldSection>();
-            UserData.RegisterType<MapTile>();
-            UserData.RegisterType<ContItem>();
-            UserData.RegisterType<LeaderItem>();
-            UserData.RegisterType<CityItem>();
-            
-            UserData.RegisterType<BldgSection>();
-            UserData.RegisterType<CtznSection>();
-            UserData.RegisterType<CultSection>();
-            UserData.RegisterType<DiffSection>();
-            UserData.RegisterType<ErasSection>();
-            UserData.RegisterType<EspnSection>();
-            UserData.RegisterType<ExprSection>();
-            UserData.RegisterType<GoodSection>();
-            UserData.RegisterType<GovtSection>();
-            UserData.RegisterType<PrtoSection>();
-            UserData.RegisterType<RaceSection>();
-            UserData.RegisterType<TechSection>();
-            UserData.RegisterType<TfrmSection>();
-            UserData.RegisterType<TerrSection>();
-            UserData.RegisterType<WsizSection>();
-            UserData.RegisterType<FlavSection>();
         }
     }
 }
