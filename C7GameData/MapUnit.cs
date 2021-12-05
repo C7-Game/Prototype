607c593328307cfe0416de85d200fc4e521bf9c9
Max <maxpet@tutanota.com>
2021-12-05 14:37:52 -0500

    Convert spaces to tabs in MapUnit.cs

--- C7GameData/MapUnit.cs
+++ C7GameData/MapUnit.cs
@@ -1,46 +1,48 @@
 namespace C7GameData
 {
-    using System;
-    using System.Collections.Generic;
-    /**
-     * A unit on the map.  Not to be confused with a unit prototype.
-     **/
-    public class MapUnit
-    {
-        public string guid  {get;}
-        public UnitPrototype unitType {get; set;}
-        public Player owner {get; set;}
-        public Tile location {get; set;}
 
-        public int movementPointsRemaining {get; set;}
-        public int hitPointsRemaining {get; set;}
-        public int maxHitPoints {
-            get {
-                return 3; // Eventually we'll add HP from experience and the type's inherent bonus
-            }
-        }
-        public bool isFortified {get; set;}
-        //sentry, etc. will come later.  For now, let's just have a couple things so we can cycle through units that aren't fortified.
+using System;
+using System.Collections.Generic;
+/**
+ * A unit on the map.  Not to be confused with a unit prototype.
+ **/
+public class MapUnit
+{
+	public string guid  {get;}
+	public UnitPrototype unitType {get; set;}
+	public Player owner {get; set;}
+	public Tile location {get; set;}
 
-        //This probably should not be serialized.  In .NET, we'd add the [ScriptIgnore] and using System.Web.Script.Serialization.
-        //But .NET Core doesn't support that.  So, we'll have to figure out something else.  Maybe a library somewhere.
-        public List<string> availableActions = new List<string>();
+	public int movementPointsRemaining {get; set;}
+	public int hitPointsRemaining {get; set;}
+	public int maxHitPoints {
+		get {
+			return 3; // Eventually we'll add HP from experience and the type's inherent bonus
+		}
+	}
+	public bool isFortified {get; set;}
+	//sentry, etc. will come later.  For now, let's just have a couple things so we can cycle through units that aren't fortified.
 
-        public MapUnit()
-        {
-            guid = Guid.NewGuid().ToString();
-        }
+	//This probably should not be serialized.  In .NET, we'd add the [ScriptIgnore] and using System.Web.Script.Serialization.
+	//But .NET Core doesn't support that.  So, we'll have to figure out something else.  Maybe a library somewhere.
+	public List<string> availableActions = new List<string>();
 
-        public override string ToString()
-        {
-            if (this != MapUnit.NONE) {
-                return unitType.name + " with " + movementPointsRemaining + " movement points and " + hitPointsRemaining + " hit points, guid = " + guid;
-            }
-            else {
-                return "This is the NONE unit";
-            }
-        }
+	public MapUnit()
+	{
+		guid = Guid.NewGuid().ToString();
+	}
 
-        public static MapUnit NONE = new MapUnit();
-    }
+	public override string ToString()
+	{
+		if (this != MapUnit.NONE) {
+			return unitType.name + " with " + movementPointsRemaining + " movement points and " + hitPointsRemaining + " hit points, guid = " + guid;
+		}
+		else {
+			return "This is the NONE unit";
+		}
+	}
+
+	public static MapUnit NONE = new MapUnit();
+}
+
 }
