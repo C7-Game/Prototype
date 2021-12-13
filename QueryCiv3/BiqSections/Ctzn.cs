3c8d14c5897f3f9e0b3d2b77e26a4f8ab75b7c1f
Lucien Maloney <hobbesdevelopment@gmail.com>
2021-12-13 08:57:48 -0600

    Added CTZN struct layout

--- QueryCiv3/BiqSections/Ctzn.cs
+++ QueryCiv3/BiqSections/Ctzn.cs
@@ -6,6 +6,43 @@ namespace QueryCiv3.Biq
     [StructLayout(LayoutKind.Sequential, Pack=1)]
     public unsafe struct CTZN
     {
+        public int Length;
+        public int DefaultCitizen;
 
+        private fixed byte Text[96];
+        public string SingularName {
+            get {
+                var Entry = new byte[32];
+                fixed (byte* p1 = Text, p2 = Entry) {
+                    Buffer.MemoryCopy(p1, p2, 32, 32);
+                }
+                return Util.GetString(Entry);
+            }
+        }
+        public string CivilopediaEntry {
+            get {
+                var Entry = new byte[32];
+                fixed (byte* p1 = Text, p2 = Entry) {
+                    Buffer.MemoryCopy(p1 + 32, p2, 32, 32);
+                }
+                return Util.GetString(Entry);
+            }
+        }
+        public string PluralName {
+            get {
+                var Entry = new byte[32];
+                fixed (byte* p1 = Text, p2 = Entry) {
+                    Buffer.MemoryCopy(p1 + 64, p2, 32, 32);
+                }
+                return Util.GetString(Entry);
+            }
+        }
+
+        public int Prerequisite;
+        public int Luxuries;
+        public int Research;
+        public int Taxes;
+        public int Corruption;
+        public int Construction;
     }
 }
