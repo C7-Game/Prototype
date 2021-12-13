7e9463eb37492c210ae7c9dedea8928e544d2d7a
Lucien Maloney <hobbesdevelopment@gmail.com>
2021-12-13 08:38:48 -0600

    Set up BIQ section data files

--- QueryCiv3/BiqSections/Bldg.cs
+++ QueryCiv3/BiqSections/Bldg.cs
@@ -1,7 +1,7 @@
 using System;
 using System.Runtime.InteropServices;
 
-namespace QueryCiv3
+namespace QueryCiv3.Biq
 {
     [StructLayout(LayoutKind.Sequential, Pack=1)]
     public unsafe struct BLDG
---
+++ QueryCiv3/BiqSections/Tfrm.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Runtime.InteropServices;
+
+namespace QueryCiv3.Biq
+{
+    [StructLayout(LayoutKind.Sequential, Pack=1)]
+    public unsafe struct TFRM
+    {
+
+    }
+}
