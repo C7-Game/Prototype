1ba6480eefc7d751c7ca5d7a6f5d47082ef6fb76
Quintillus <quintillus@warpmail.net>
2022-02-06 03:36:52 -0500

    Start adding experimental AIs for players, and units in particular.  Set up some structure around storing unit objectives, for various plausible objectives.  In TurnInteractions, non-human players now set AI goals for units, and then execute on those goals.
    It's still not good, but the players do build their first city, and move units around.  It also sets the stage for things like teaching the AI how to choose settler destinations for their second-and-subsequent settlers, and thus moving towards a destination instead of randomly.
    
    Before enhancing the AI much, I'm probably going to look at some display details like getting city colors to match unit colors, getting unit colors to line up properly, setting the animation direction properly for AI units, and maybe adding some basic city name lists.

--- C7Engine/EntryPoints/TurnHandling.cs
+++ C7Engine/EntryPoints/TurnHandling.cs
@@ -1,3 +1,5 @@
+using C7GameData.AIData;
+
 namespace C7Engine
 {
     using C7GameData;
@@ -45,6 +47,60 @@ namespace C7Engine
             BarbarianAI barbarianAI = new BarbarianAI();
             barbarianAI.PlayTurn(gameData.players[0], gameData);
 
+			//Non-Barbarian AIs
+			foreach (Player player in gameData.players)
+			{
+				if (player.isHuman || player.isBarbarians) {
+					continue;
+				}
+				//Do things with units.  Copy into an array first to avoid collection-was-modified exception
+				foreach (MapUnit unit in player.units.ToArray())
+				{
+					if (unit.currentAIBehavior == null) {
+						SetAIForUnit(unit);
+					}
+					
+					//Now actually take actions
+					//TODO: Move these into an AI method
+					if (unit.currentAIBehavior is SettlerAI settlerAi) {
+						if (unit.location == settlerAi.destination) {
+							Console.WriteLine("Building city with " + unit);
+							CityInteractions.BuildCity(unit.location.xCoordinate, unit.location.yCoordinate, player.guid, "Neo-Tokyo");
+							UnitInteractions.disbandUnit(unit.guid);
+						}
+						else {
+							//TODO: Move towards the destination
+							Console.WriteLine("Moving settler unit towards " + settlerAi.destination);
+						}
+					}
+					else if (unit.currentAIBehavior is DefenderAI defenderAI) {
+						if (defenderAI.destination == unit.location) {
+							UnitInteractions.fortifyUnit(unit.guid);
+							Console.WriteLine("Fortifying " + unit + " at " + defenderAI.destination);
+						}
+						else {
+							//TODO: Move towards destination
+							Console.WriteLine("Moving defender towards " + defenderAI.destination);
+						}
+					}
+					else if (unit.currentAIBehavior is ExplorerAI explorerAi) {
+						Console.Write("Moving explorer AI for " + unit);
+						//TODO: Distinguish between types of exploration
+						//TODO: Make sure ON_A_BOAT units stay on the boat
+						//Move randomly
+						Tile newLocation = unit.unitType is SeaUnit ? unit.location.RandomCoastNeighbor() : unit.location.RandomLandNeighbor();
+						//Because it chooses a semi-cardinal direction at random, not accounting for map, it could get none
+						//if it tries to move e.g. north from the north pole.  Hence, this check.
+						if (newLocation != Tile.NONE) {
+							Console.WriteLine("Moving unit at " + unit.location + " to " + newLocation);
+							unit.location.unitsOnTile.Remove(unit);
+							newLocation.unitsOnTile.Add(unit);
+							unit.location = newLocation;
+						}
+					}
+				}
+			}
+
             //City Production
             foreach (City city in gameData.cities)
             {
@@ -73,6 +129,45 @@ namespace C7Engine
             //at the same place in the order.  Confirmed this is what Civ3 does.
             UnitInteractions.ClearWaitQueue();
             gameData.turn++;
+        }
+        private static void SetAIForUnit(MapUnit unit)
+        {
+
+	        //figure out an AI behavior
+	        //TODO: Use strategies, not names
+	        if (unit.unitType.name == "Settler") {
+		        SettlerAI settlerAI = new SettlerAI();
+		        settlerAI.goal = SettlerAI.SettlerGoal.BUILD_CITY;
+		        //TODO: Better way of finding a destination
+		        //TODO: Find a destination if there's already a city here.
+		        settlerAI.destination = unit.location;
+		        Console.WriteLine("Set AI for unit to settler AI with destination of " + settlerAI.destination);
+		        unit.currentAIBehavior = settlerAI;
+	        }
+	        else if (unit.location.cityAtTile != null && unit.location.unitsOnTile.Count == 0) {
+		        DefenderAI ai = new DefenderAI();
+		        ai.goal = DefenderAI.DefenderGoal.DEFEND_CITY;
+		        ai.destination = unit.location;
+		        Console.WriteLine("Set defender AI for " + unit + " with destination of " + ai.destination);
+		        unit.currentAIBehavior = ai;
+	        }
+	        else {
+		        ExplorerAI ai = new ExplorerAI();
+		        if (unit.unitType is SeaUnit) {
+			        ai.type = ExplorerAI.ExplorationType.COASTLINE;
+			        Console.WriteLine("Set coastline exploration AI for " + unit);
+		        }
+		        else if (unit.location.unitsOnTile.Exists((x) => x.unitType is SeaUnit)) {
+			        ai.type = ExplorerAI.ExplorationType.ON_A_BOAT;
+			        //TODO: Actually put the unit on the boat
+			        Console.WriteLine("Set ON_A_BOAT exploration AI for " + unit);
+		        }
+		        else {
+			        ai.type = ExplorerAI.ExplorationType.RANDOM;
+			        Console.WriteLine("Set random exploration AI for " + unit);
+		        }
+		        unit.currentAIBehavior = ai;
+	        }
         }
 
         ///Eventually we'll have a game year or month or whatever, but for now this provides feedback on our progression
---
+++ C7GameData/AIData/ExplorerAI.cs
@@ -0,0 +1,15 @@
+namespace C7GameData.AIData
+{
+	public class ExplorerAI : UnitAI
+	{
+		public enum ExplorationType
+		{
+			RANDOM,
+			COASTLINE,
+			DIRECTIONAL,
+			SCOUT_RIVAL,
+			ON_A_BOAT
+		}
+		public ExplorationType type;
+	}
+}
