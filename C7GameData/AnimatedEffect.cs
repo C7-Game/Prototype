118e862d0d76add71477a376825fc9f5834e392f
Max <maxpet@tutanota.com>
2022-03-10 20:06:32 -0500

    Comment explaining AnimatedEffect enum values & plans for future development

--- C7GameData/AnimatedEffect.cs
+++ C7GameData/AnimatedEffect.cs
@@ -3,6 +3,9 @@
 // can't be guessed based on the enum names (like MapUnit.AnimatedAction) because the files aren't all stored in predictable folder locations.
 namespace C7GameData
 {
+// These enum values match the names of animation files in civ3PTW/Art/Animations/Trajectory. The actual mapping from enum value to file name is
+// determined by a dictionary in the Civ3Anim class (in Civ3AnimData.cs, part of the UI). TODO: Ultimately we'll want a way for the engine to specify
+// effect animations that is extensible for modders and not limited to Civ 3's animations.
 public enum AnimatedEffect {
 	Hit,
 	Hit2,
