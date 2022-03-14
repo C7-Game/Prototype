1148d2461fa6394d7424c2595023a4b46594920e
Travis Christian <wildweazel@gmail.com>
2022-03-13 23:22:33 -0500

    Add credits file to display in the Credits scene

--- C7/Credits.cs
+++ C7/Credits.cs
@@ -1,13 +1,14 @@
 using Godot;
-using ConvertCiv3Media;
 
 public class Credits : Node2D
 {
+	private string creditsText;
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		GD.Print("Now rolling the credits!");
+		creditsText = System.IO.File.ReadAllText("./credits.txt");
 		ShowCredits();
 	}
 
@@ -20,10 +21,11 @@ public class Credits : Node2D
 		CreditsBackground.Texture = CreditsTexture;
 		AddChild(CreditsBackground);
 
-		//Todo: Either import Text/credits.txt and scroll it as done in-game, or have our own custom credits.
 		Label creditsLabel = new Label();
-		creditsLabel.Text = "Project Ringleader: WildWeazel";
-		creditsLabel.SetPosition(new Vector2(360, 120));
+		creditsLabel.Text = creditsText;
+		creditsLabel.Align = Label.AlignEnum.Center;
+		creditsLabel.RectPosition = new Vector2(80, 120);
+		creditsLabel.RectSize = new Vector2(864, 528);
 		AddChild(creditsLabel);
 		
 		TextureButton GoBackButton = new TextureButton();
