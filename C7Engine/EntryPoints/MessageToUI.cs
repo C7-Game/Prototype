3ddf2a857cdd99a085ff2f8628b9bb13768afda1
Max <maxpet@tutanota.com>
2022-03-28 21:30:04 -0400

    Put default ending param in MapUnit.animate instead of the animation message ctors

--- C7Engine/EntryPoints/MessageToUI.cs
+++ C7Engine/EntryPoints/MessageToUI.cs
@@ -17,7 +17,7 @@ public class MsgStartUnitAnimation : MessageToUI {
 	public AutoResetEvent completionEvent;
 	public AnimationEnding ending;
 
-	public MsgStartUnitAnimation(MapUnit unit, MapUnit.AnimatedAction action, AutoResetEvent completionEvent, AnimationEnding ending = AnimationEnding.Stop)
+	public MsgStartUnitAnimation(MapUnit unit, MapUnit.AnimatedAction action, AutoResetEvent completionEvent, AnimationEnding ending)
 	{
 		this.unitGUID = unit.guid;
 		this.action = action;
@@ -32,7 +32,7 @@ public class MsgStartEffectAnimation : MessageToUI {
 	public AutoResetEvent completionEvent;
 	public AnimationEnding ending;
 
-	public MsgStartEffectAnimation(Tile tile, AnimatedEffect effect, AutoResetEvent completionEvent, AnimationEnding ending = AnimationEnding.Stop)
+	public MsgStartEffectAnimation(Tile tile, AnimatedEffect effect, AutoResetEvent completionEvent, AnimationEnding ending)
 	{
 		this.tileIndex = EngineStorage.gameData.map.tileCoordsToIndex(tile.xCoordinate, tile.yCoordinate);
 		this.effect = effect;
