a5732a7f335b2e5453bc0a8341509b59323bfe8b
Paul Cento <paul.cento@gmail.com>
2022-04-01 22:38:36 -0400

    add zip compression for save files

--- C7GameData/SaveFormat.cs
+++ C7GameData/SaveFormat.cs
@@ -10,27 +10,59 @@ namespace C7GameData
 */
 {
     using System.IO;
+    using System.IO.Compression;
     using System.Text.Json;
+
+    public enum SaveFileFormat
+    {
+        Plain,
+        Zip,
+    }
+
     public class C7SaveFormat
     {
         public string Version = "v0.0early-prototype";
+
         // Rules is intended to be the analog to a BIC/X/Q
         public C7RulesFormat Rules;
+
         // This naming is probably bad form, but it makes sense to me to name it as such here
         public GameData GameData;
+
         public C7SaveFormat()
         {
 	        GameData = new GameData();
         }
+
         public C7SaveFormat(GameData gameData, C7RulesFormat rules = null)
         {
-            this.GameData = gameData;
+            GameData = gameData;
             Rules = rules;
         }
-        public static C7SaveFormat Load(string path)
+
+        public static C7SaveFormat Load(string path, SaveFileFormat format = SaveFileFormat.Plain)
         {
-            string json = File.ReadAllText(path);
-            C7SaveFormat save = JsonSerializer.Deserialize<C7SaveFormat>(json, JsonOptions);
+            System.Console.WriteLine("loading...");
+            C7SaveFormat save = null;
+            if (format == SaveFileFormat.Plain)
+            {
+                string json = File.ReadAllText(path);
+                save = JsonSerializer.Deserialize<C7SaveFormat>(json, JsonOptions);
+            }
+            else
+            {
+                using (var fileStream = new FileStream(path, FileMode.Open))
+                using (var archive = new ZipArchive(fileStream, ZipArchiveMode.Read))
+                {
+                    ZipArchiveEntry entry = archive.GetEntry("save");
+                    using (Stream stream = entry.Open())
+                    {
+                        var task = JsonSerializer.DeserializeAsync<C7SaveFormat>(stream, JsonOptions).AsTask();
+                        save = task.GetAwaiter().GetResult();
+                    }
+                }
+            }
+
             //Inflate things that are stored by reference
             foreach (Tile tile in save.GameData.map.tiles) {
                 if (tile.ResourceKey == "NONE") {
@@ -44,11 +76,30 @@ namespace C7GameData
             }
             return save;
         }
-        public static void Save(C7SaveFormat save, string path)
+
+        public static void Save(C7SaveFormat save, string path, SaveFileFormat format = SaveFileFormat.Plain)
         {
-            string json = JsonSerializer.Serialize(save, JsonOptions);
-            File.WriteAllText(path, json);
+            byte[] json = JsonSerializer.SerializeToUtf8Bytes(save, JsonOptions);
+            if (format == SaveFileFormat.Zip)
+            {
+                using (var zipStream = new MemoryStream())
+                {
+                    var archive = new ZipArchive(zipStream, ZipArchiveMode.Create);
+                    ZipArchiveEntry entry = archive.CreateEntry("save");
+                    using (Stream stream = entry.Open())
+                    {
+                        stream.Write(json, 0, json.Length);
+                    }
+                    archive.Dispose();
+                    File.WriteAllBytes(path, zipStream.ToArray());
+                }
+            }
+            else
+            {
+                File.WriteAllBytes(path, json);
+            }
         }
+
         public static JsonSerializerOptions JsonOptions { get => new JsonSerializerOptions
         {
                 // Lower-case the first letter in JSON because JSON naming standards
