6722fb9b3c5e7e6ad43bf07f4d7d0969c8b18a8c
Paul Cento <paul.cento@gmail.com>
2022-04-14 17:30:26 -0400

    fix TilePath not deserializing

--- C7GameData/AIData/TilePath.cs
+++ C7GameData/AIData/TilePath.cs
@@ -8,7 +8,7 @@ namespace C7GameData
 		private Tile destination; //stored in case we need to re-calculate
 		private Queue<Tile> path;
 
-		private TilePath() {
+		public TilePath() {
 			destination = Tile.NONE;
 			path = new Queue<Tile>();
 		}
