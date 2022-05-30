d04c556e6fdc8876980c670c86d5dc68f140f16c
Quintillus <quintillus@warpmail.net>
2022-05-30 03:19:24 -0400

    Add a StrategicPriority structure, and a first StrategicPriority (ExpansionPriority) that follows that structure.
    Once another priority or two are added, the next step will be having the AI choose a priority.  This will be the first "arbiter" added to the AI, at least with a planned structure.  CityLocationAI kind of does arbitrate, but doesn't have a generic, pluggable architecture for that.  StrategicPriorityAI will.

---
+++ C7Engine/AI/StrategicAI/ExpansionPriority.cs
@@ -0,0 +1,47 @@
+using System.Collections.Generic;
+using C7Engine;
+
+namespace C7GameData.AIData {
+	public class ExpansionPriority : StrategicPriority {
+		private readonly int TEMP_GAME_LENGTH = 540;
+		private readonly int EARLY_GAME_CUTOFF = 25;	//what percentage of the game is early game, which should give expansion a boost?
+
+		public float GetWeight(Player player) {
+			if (player.cities.Count < 2) {
+				return 1000;
+			} else {
+				int score = CalculateAvailableLandScore(player);
+
+				score = ApplyEarlyGameMultiplier(score);
+				score = ApplyNationTraitMultiplier(score, player);
+				return score;
+			}
+		}
+		private static int CalculateAvailableLandScore(Player player)
+		{
+			//Figure out if there's land to settle, and how much
+			Dictionary<Tile, int> possibleLocations = SettlerLocationAI.GetPossibleNewCityLocations(player.cities[0].location, player);
+			int score = possibleLocations.Count * 10;
+			foreach (int i in possibleLocations.Values) {
+				score += i / 10;
+			}
+			return score;
+		}
+		private int ApplyEarlyGameMultiplier(int score)
+		{
+			//If it's early game, multiply this score.
+			//TODO: We haven't implemented the part for "how many turns does the game have?" yet.  So this is hard-coded.
+			int gameTurn = EngineStorage.gameData.turn;
+			int percentOfGameFinished = (gameTurn * 100) / TEMP_GAME_LENGTH;
+			if (percentOfGameFinished < 25) {
+				score = score * (25 - percentOfGameFinished) / 5;
+			}
+			return score;
+		}
+
+		private int ApplyNationTraitMultiplier(int score, Player player) {
+			// TODO: The "Expansionist" trait should give a higher priority to this strategic priority.
+			return score;
+		}
+	}
+}
