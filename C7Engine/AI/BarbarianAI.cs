b14763a0b6223f2d066e4b15fb3d8909ada4c4c3
Quintillus <quintillus@warpmail.net>
2022-07-21 17:42:07 -0400

    The barbarian AI will now use all three movement points on its galleys.

--- C7Engine/AI/BarbarianAI.cs
+++ C7Engine/AI/BarbarianAI.cs
@@ -21,19 +21,25 @@ namespace C7Engine {
 			// issue for the barbs but will be for similar loops elsewhere in the AI logic.
 			foreach (MapUnit unit in player.units.ToArray()) {
 				if (UnitIsFreeToMove(unit)) {
-					//Move randomly
-					List<Tile> validTiles = unit.unitType.categories.Contains("Sea") ? unit.location.GetCoastNeighbors() : unit.location.GetLandNeighbors();
-					if (validTiles.Count == 0) {
-						//This can happen if a barbarian galley spawns next to a 1-tile lake, moves there, and doesn't have anywhere else to go.
-						log.Warning("WARNING: No valid tiles for barbarian to move to");
-						continue;
-					}
-					Tile newLocation = validTiles[GameData.rng.Next(validTiles.Count)];
-					//Because it chooses a semi-cardinal direction at random, not accounting for map, it could get none
-					//if it tries to move e.g. north from the north pole.  Hence, this check.
-					if (newLocation != Tile.NONE) {
-						log.Debug("Moving barbarian at " + unit.location + " to " + newLocation);
-						unit.move(unit.location.directionTo(newLocation));
+					while (unit.movementPointsRemaining > 0) {
+						//Move randomly
+						List<Tile> validTiles = unit.unitType.categories.Contains("Sea") ? unit.location.GetCoastNeighbors() : unit.location.GetLandNeighbors();
+						if (validTiles.Count == 0) {
+							//This can happen if a barbarian galley spawns next to a 1-tile lake, moves there, and doesn't have anywhere else to go.
+							log.Warning("WARNING: No valid tiles for barbarian to move to");
+							continue;
+						}
+						Tile newLocation = validTiles[GameData.rng.Next(validTiles.Count)];
+						//Because it chooses a semi-cardinal direction at random, not accounting for map, it could get none
+						//if it tries to move e.g. north from the north pole.  Hence, this check.
+						if (newLocation != Tile.NONE) {
+							log.Debug("Moving barbarian at " + unit.location + " to " + newLocation);
+							unit.move(unit.location.directionTo(newLocation));
+							unit.movementPointsRemaining -= newLocation.MovementCost();
+						} else {
+							//Avoid potential infinite loop.
+							break;
+						}
 					}
 				}
 			}
