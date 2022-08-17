fb6008d16852e058f8efa2fb206be58cffe22428
Quintillus <quintillus@warpmail.net>
2022-08-17 11:41:36 -0400

    Refactor CityLayer so the mega method is broken into a few helper methods.
    Functionally it should be identical.

--- C7/Map/CityLayer.cs
+++ C7/Map/CityLayer.cs
@@ -19,6 +19,11 @@ namespace C7.Map {
 		private Pcx cityIcons = Util.LoadPCX("Art/Cities/city icons.pcx");
 		private Image nonEmbassyStar;
 
+		const int CITY_LABEL_HEIGHT = 23;
+		const int LEFT_RIGHT_BOXES_WIDTH = 24;
+		const int LEFT_RIGHT_BOXES_HEIGHT = CITY_LABEL_HEIGHT - 2;
+		const int TEXT_ROW_HEIGHT = 9;
+
 		public CityLayer()
 		{
 			//TODO: Generalize, support multiple city types, etc.
@@ -46,8 +51,8 @@ namespace C7.Map {
 
 			int turnsUntilGrowth = city.TurnsUntilGrowth();
 			string turnsUntilGrowthText = turnsUntilGrowth == int.MaxValue ? "- -" : "" + turnsUntilGrowth;
-			String cityNameAndGrowth = $"{city.name} : {turnsUntilGrowthText}";
-			String productionDescription = city.itemBeingProduced.name + " : " + city.TurnsUntilProductionFinished();
+			string cityNameAndGrowth = $"{city.name} : {turnsUntilGrowthText}";
+			string productionDescription = city.itemBeingProduced.name + " : " + city.TurnsUntilProductionFinished();
 
 			int cityNameAndGrowthWidth = (int)smallFont.GetStringSize(cityNameAndGrowth).x;
 			int productionDescriptionWidth = (int)smallFont.GetStringSize(productionDescription).x;
@@ -60,19 +65,55 @@ namespace C7.Map {
 				log.Verbose("City label width: " + cityLabelWidth);
 				log.Verbose("Text area width: " + textAreaWidth);
 			}
-			const int CITY_LABEL_HEIGHT = 23;
-			const int TEXT_ROW_HEIGHT = 9;
-			const int LEFT_RIGHT_BOXES_WIDTH = 24;
-			const int LEFT_RIGHT_BOXES_HEIGHT = CITY_LABEL_HEIGHT - 2;
 
+			Image labelBackground = CreateLabelBackground(cityLabelWidth, city, textAreaWidth);
+			ImageTexture cityLabel = new ImageTexture();
+			cityLabel.CreateFromImage(labelBackground, 0);
+
+			DrawLabelOnScreen(looseView, tileCenter, cityLabelWidth, city, cityLabel);
+			DrawTextOnLabel(looseView, tileCenter, cityNameAndGrowthWidth, productionDescriptionWidth, city, cityNameAndGrowth, productionDescription, cityLabelWidth);
+		}
+		private void DrawLabelOnScreen(LooseView looseView, Vector2 tileCenter, int cityLabelWidth, City city, ImageTexture cityLabel)
+		{
+
+			Rect2 labelDestination = new Rect2(tileCenter + new Vector2(cityLabelWidth / -2, 24), new Vector2(cityLabelWidth, CITY_LABEL_HEIGHT)); //24 is a swag
+			Rect2 allOfTheLabel = new Rect2(new Vector2(0, 0), new Vector2(cityLabelWidth, CITY_LABEL_HEIGHT));
+			cityLabels[city.name] = cityLabel;
+			looseView.DrawTextureRectRegion(cityLabel, labelDestination, allOfTheLabel);
+		}
+
+		private void DrawTextOnLabel(LooseView looseView, Vector2 tileCenter, int cityNameAndGrowthWidth, int productionDescriptionWidth, City city, string cityNameAndGrowth, string productionDescription, int cityLabelWidth)
+		{
+			//Destination for font is based on lower-left of baseline of font, not upper left as for blitted rectangles
+			int cityNameOffset = cityNameAndGrowthWidth / -2;
+			int prodDescriptionOffset = productionDescriptionWidth / -2;
+			if (!city.IsCapital()) {
+				cityNameOffset += 12;
+				prodDescriptionOffset += 12;
+			}
+			Vector2 cityNameDestination = new Vector2(tileCenter + new Vector2(cityNameOffset, 24) + new Vector2(0, 10));
+			looseView.DrawString(smallFont, cityNameDestination, cityNameAndGrowth, Color.Color8(255, 255, 255, 255));
+			Vector2 productionDestination = new Vector2(tileCenter + new Vector2(prodDescriptionOffset, 24) + new Vector2(0, 20));
+			looseView.DrawString(smallFont, productionDestination, productionDescription, Color.Color8(255, 255, 255, 255));
+
+			//City pop size
+			string popSizeString = "" + city.size;
+			int popSizeWidth = (int)midSizedFont.GetStringSize(popSizeString).x;
+			int popSizeOffset = LEFT_RIGHT_BOXES_WIDTH / 2 - popSizeWidth / 2;
+			Vector2 popSizeDestination = new Vector2(tileCenter + new Vector2(cityLabelWidth / -2, 24) + new Vector2(popSizeOffset, 18));
+			looseView.DrawString(midSizedFont, popSizeDestination, popSizeString, Color.Color8(255, 255, 255, 255));
+		}
+
+		private Image CreateLabelBackground(int cityLabelWidth, City city, int textAreaWidth)
+		{
 			//Label/name/producing area
 			Image labelImage = new Image();
 			labelImage.Create(cityLabelWidth, CITY_LABEL_HEIGHT, false, Image.Format.Rgba8);
 			labelImage.Fill(Color.Color8(0, 0, 0, 0));
-			byte transparencyLevel = 192;	//25%
+			byte transparencyLevel = 192; //25%
 			Color civColor = new Color(city.owner.color);
 			civColor = new Color(civColor, transparencyLevel);
-			Color civColorDarker = Color.Color8(0, 0, 138, transparencyLevel);	//todo: automate the darker() function.  maybe less transparency?
+			Color civColorDarker = Color.Color8(0, 0, 138, transparencyLevel); //todo: automate the darker() function.  maybe less transparency?
 			Color topRowGrey = Color.Color8(32, 32, 32, transparencyLevel);
 			Color bottomRowGrey = Color.Color8(48, 48, 48, transparencyLevel);
 			Color backgroundGrey = Color.Color8(64, 64, 64, transparencyLevel);
@@ -119,35 +160,8 @@ namespace C7.Map {
 				labelImage.BlitRect(leftAndRightBoxes, new Rect2(0, 0, new Vector2(24, 21)), new Vector2(cityLabelWidth - 25, 1));
 				labelImage.BlendRect(nonEmbassyStar, new Rect2(0, 0, new Vector2(18, 18)), new Vector2(cityLabelWidth - 24, 2));
 			}
-
 			//todo: darker shades of civ color around edges
-
-			ImageTexture cityLabel = new ImageTexture();
-			cityLabel.CreateFromImage(labelImage, 0);
-
-			Rect2 labelDestination = new Rect2(tileCenter + new Vector2(cityLabelWidth/-2, 24), new Vector2(cityLabelWidth, CITY_LABEL_HEIGHT));	//24 is a swag
-			Rect2 allOfTheLabel = new Rect2(new Vector2(0, 0), new Vector2(cityLabelWidth, CITY_LABEL_HEIGHT));
-			cityLabels[city.name] = cityLabel;
-			looseView.DrawTextureRectRegion(cityLabel, labelDestination, allOfTheLabel);
-
-			//Destination for font is based on lower-left of baseline of font, not upper left as for blitted rectangles
-			int cityNameOffset = cityNameAndGrowthWidth/-2;
-			int prodDescriptionOffset = productionDescriptionWidth/-2;
-			if (!city.IsCapital()) {
-				cityNameOffset+=12;
-				prodDescriptionOffset+=12;
-			}
-			Vector2 cityNameDestination = new Vector2(tileCenter + new Vector2(cityNameOffset, 24) + new Vector2(0, 10));
-			looseView.DrawString(smallFont, cityNameDestination, cityNameAndGrowth, Color.Color8(255, 255, 255, 255));
-			Vector2 productionDestination = new Vector2(tileCenter + new Vector2(prodDescriptionOffset, 24) + new Vector2(0, 20));
-			looseView.DrawString(smallFont, productionDestination, productionDescription, Color.Color8(255, 255, 255, 255));
-
-			//City pop size
-			string popSizeString = "" + city.size;
-			int popSizeWidth = (int)midSizedFont.GetStringSize(popSizeString).x;
-			int popSizeOffset = LEFT_RIGHT_BOXES_WIDTH/2 - popSizeWidth/2;
-			Vector2 popSizeDestination = new Vector2(tileCenter + new Vector2(cityLabelWidth/-2, 24) + new Vector2(popSizeOffset, 18));
-			looseView.DrawString(midSizedFont, popSizeDestination, popSizeString, Color.Color8(255, 255, 255, 255));
+			return labelImage;
 		}
 	}
 }
