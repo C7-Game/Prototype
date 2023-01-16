fff8f821fa432d4a9406f5d44b97ccadfa5f515b
Quintillus <quintillus@warpmail.net>
2023-01-16 14:06:49 -0500

    370 - Fix the centering of the city image so it works on city graphics of arbitrary size.

--- C7/Map/CityLayer.cs
+++ C7/Map/CityLayer.cs
@@ -28,8 +28,11 @@ namespace C7.Map {
 		{
 			//TODO: Generalize, support multiple city types, etc.
 			//TODO: Centered city art texture
-			cityTexture = Util.LoadTextureFromPCX("Art/Cities/rMIDEAST.PCX", 0, 0, 167, 95);
-			citySpriteSize = new Vector2(167, 95);
+			Pcx pcx = Util.LoadPCX("Art/Cities/rMIDEAST.PCX");
+			int height = pcx.Height/4;
+			int width = pcx.Width/3;
+			cityTexture = Util.LoadTextureFromPCX("Art/Cities/rMIDEAST.PCX", 0, 0, width, height);
+			citySpriteSize = new Vector2(width, height);
 			smallFont.FontData = ResourceLoader.Load<DynamicFontData>("res://Fonts/NotoSans-Regular.ttf");
 			smallFont.Size = 11;
 
