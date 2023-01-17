9fdd702352bf94ad5eb372a4837a7e296e323e03
Quintillus <quintillus@warpmail.net>
2023-01-16 23:46:09 -0500

    #172 - Move everything over into a scene.
    Unfortunately this is not an improvement, you can see that it never removes things it formerly drew, and this means things get really slow.  I am not yet sure why that is.

--- C7/Map/CityLayer.cs
+++ C7/Map/CityLayer.cs
@@ -9,30 +9,18 @@ using Serilog.Events;
 namespace C7.Map {
 	public class CityLayer : LooseLayer {
 
-		private ILogger log = LogManager.ForContext<Game>();
+		private ILogger log = LogManager.ForContext<CityLayer>();
 
 		private ImageTexture cityTexture;
 		private Dictionary<string, ImageTexture> cityLabels = new Dictionary<string, ImageTexture>();
 
-		private DynamicFont smallFont = new DynamicFont();
-		private DynamicFont midSizedFont = new DynamicFont();
-		private Pcx cityIcons = Util.LoadPCX("Art/Cities/city icons.pcx");
-		private Image nonEmbassyStar;
 
-		const int CITY_LABEL_HEIGHT = 23;
-		const int LEFT_RIGHT_BOXES_WIDTH = 24;
-		const int LEFT_RIGHT_BOXES_HEIGHT = CITY_LABEL_HEIGHT - 2;
-		const int TEXT_ROW_HEIGHT = 9;
+
 
 		public CityLayer()
 		{
-			smallFont.FontData = ResourceLoader.Load<DynamicFontData>("res://Fonts/NotoSans-Regular.ttf");
-			smallFont.Size = 11;
 
-			midSizedFont.FontData = ResourceLoader.Load<DynamicFontData>("res://Fonts/NotoSans-Regular.ttf");
-			midSizedFont.Size = 18;
 
-			nonEmbassyStar = PCXToGodot.getImageFromPCX(cityIcons, 20, 1, 18, 18);
 		}
 
 		public override void drawObject(LooseView looseView, GameData gameData, Tile tile, Vector2 tileCenter)
