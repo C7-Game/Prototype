cb6a5810933f256fe2b06bd1491b39f92a8f1b8b
Paul Cento <paul.cento@gmail.com>
2023-02-19 20:58:42 -0500

    add nuget package for encoding

--- C7/Util.cs
+++ C7/Util.cs
@@ -5,9 +5,10 @@ using System.Runtime.InteropServices;
 using Godot;
 using ConvertCiv3Media;
 using C7GameData;
+using QueryCiv3;
 
 public partial class Util {
-	static public string Civ3Root = GetCiv3Path();
+	static public string Civ3Root = Civ3Location.GetCiv3Path();
 	public partial class Civ3FileDialog : FileDialog
 	// Use this instead of a scene-based FileDialog to avoid it saving the local dev's last browsed folder in the repo
 	// While instantiated it will return to the last-accessed folder when reopened
@@ -25,45 +26,6 @@ public partial class Util {
 			// OffsetBottom = 750;
 			base._Ready();
 		}
-	}
-
-	static private string SteamCommonDir() {
-		string home = System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal);
-		if (home == null) { return null; }
-		return System.IO.Path.Combine(home, "Library/Application Support/Steam/steamapps/common");
-	}
-
-	static private bool FolderIsCiv3(System.IO.DirectoryInfo di) {
-		return di.EnumerateFiles().Any(f => f.Name == "civ3id.mb");
-	}
-
-	static public string GetCiv3Path() {
-		// Use CIV3_HOME env var if present
-		string path = System.Environment.GetEnvironmentVariable("CIV3_HOME");
-		if (path != null) { return path; }
-
-		if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows)) {
-			// Look up in Windows registry if present
-			path = Civ3PathFromRegistry();
-			if (path != null) { return path; }
-		} else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX) || RuntimeInformation.IsOSPlatform(OSPlatform.Linux)) {
-			// Check for a civ3 folder in steamapps/common
-			System.IO.DirectoryInfo root = new System.IO.DirectoryInfo(Util.SteamCommonDir());
-			foreach (System.IO.DirectoryInfo di in root.GetDirectories()) {
-				if (Util.FolderIsCiv3(di)) {
-					return di.FullName;
-				}
-			}
-		}
-
-		return "/civ3/path/not/found";
-	}
-
-	static public string Civ3PathFromRegistry() {
-		// Assuming 64-bit platform, get vanilla Civ3 install folder from registry
-		// Return null if value not present or if key not found
-		object path = Microsoft.Win32.Registry.GetValue(@"HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\Infogrames Interactive\Civilization III", "install_path", null);
-		return path == null ? null : (string)path;
 	}
 
 	// Checks if a file exists ignoring case on the latter parts of its path. If the file is found, returns its full path re-capitalized as
