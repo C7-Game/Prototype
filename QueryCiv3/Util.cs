05a0862c6bd5897db63532d6db254c208d1cc1e9
Paul Cento <paul.cento@gmail.com>
2023-09-19 23:34:57 -0400

    more cleanup

--- QueryCiv3/Util.cs
+++ QueryCiv3/Util.cs
@@ -17,14 +17,14 @@ namespace QueryCiv3 {
 
 	public class Util {
 		// Encoding code page ID; 1252 is Civ3 encoding for US language version
-		static public Encoding Civ3Encoding;
+		public static Encoding Civ3Encoding;
 
 		static Util() {
 			Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
 			Civ3Encoding = Encoding.GetEncoding(1252);
 		}
 
-		static public byte[] ReadFile(string pathName) {
+		public static byte[] ReadFile(string pathName) {
 			byte[] MyFileData = File.ReadAllBytes(pathName);
 			if (MyFileData[0] == 0x00 && (MyFileData[1] == 0x04 || MyFileData[1] == 0x05 || MyFileData[1] == 0x06)) {
 				return Decompress(MyFileData);
@@ -32,30 +32,30 @@ namespace QueryCiv3 {
 			return MyFileData;
 		}
 
-		static public byte[] Decompress(byte[] compressedBytes) {
+		public static byte[] Decompress(byte[] compressedBytes) {
 			MemoryStream DecompressedStream = new MemoryStream();
 			BlastDecoder Decompressor = new BlastDecoder(new MemoryStream(compressedBytes, writable: false), DecompressedStream);
 			Decompressor.Decompress();
 			return DecompressedStream.ToArray();
 		}
 
-		static public string GetString(byte[] bytes) {
+		public static string GetString(byte[] bytes) {
 			string Out = Civ3Encoding.GetString(bytes);
 			Regex TrimAfterNull = new Regex(@"^[^\0]*");
 			Match NoNullMatch = TrimAfterNull.Match(Out);
 			return NoNullMatch.Value;
 		}
 
-		static public unsafe string GetString<T>(ref T structData, int start, int length) where T : unmanaged {
-			var Arr = new byte[length];
+		public static unsafe string GetString<T>(ref T structData, int start, int length) where T : unmanaged {
+			byte[] Arr = new byte[length];
 			fixed (void* dataPtr = &structData, arrPtr = Arr) {
 				Buffer.MemoryCopy(((byte*)dataPtr) + start, (byte*)arrPtr, length, length);
 			}
-			return Util.GetString(Arr);
+			return GetString(Arr);
 		}
 
-		static public bool GetFlag(byte flags, int index) {
-			return (((flags >> index) & 1) == 1);
+		public static bool GetFlag(byte flags, int index) {
+			return ((flags >> index) & 1) == 1;
 		}
 	}
 }
